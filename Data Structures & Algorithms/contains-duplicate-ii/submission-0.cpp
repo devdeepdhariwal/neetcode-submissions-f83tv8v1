156235f219d4f2204afa0610205c7bd152ca6554
Devdeep Dhariwal <168007504+devdeepdhariwal@users.noreply.github.com>
2026-09-21 20:20:39 +0530

    Add: contains-duplicate-ii - submission-0

---
+++ Data Structures & Algorithms/contains-duplicate-ii/submission-0.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        unordered_map<int, int> mp;
+
+        for(int i = 0; i<nums.size(); i++){
+            if(mp.count(nums[i])){
+             auto it = mp.find(nums[i]);
+            if (it != mp.end()) {
+             int index = it->second;
+             if(abs(index-i)<=k){
+                return true;
+             }
+                                }
+            }
+            mp[nums[i]] = i;
+
+        }
+        return false;
+    }
+};
